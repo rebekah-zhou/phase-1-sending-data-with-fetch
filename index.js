c0f5d38873c127702fb918c7fa3f60b0c8f984c4
Rebekah Zhou <rebekahzhou@gmail.com>
2022-01-20 16:04:15 -0800

    commit tree 9d3aefb087b5a6228ffa0d9ddcbb9520e92487ee

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name1, email1) {
+    const url = 'http://localhost:3000/users'
+    const obj = {
+        name: name1,
+        email: email1  
+    }
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(obj)
+    }
+
+    return fetch(url, configObj)
+        .then(res => res.json)
+        .then(data => console.log(data))
+}
